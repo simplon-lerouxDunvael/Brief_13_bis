a6098f810716de6a058b7c372d70b3833bb2a9c1
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-10-02 13:57:35 +0200

    readme added

--- output.tf
+++ output.tf
@@ -10,10 +10,6 @@ output "client_certificate" {
   value = azurerm_kubernetes_cluster.AKS.kube_config.0.client_certificate
 }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.AKS.kube_config_raw
-}
-
 output "priv_subnet_id" {
   value = azurerm_subnet.priv_subnet.id
 }
